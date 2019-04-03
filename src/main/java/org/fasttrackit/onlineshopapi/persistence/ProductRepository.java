a57cb93e912e618ba4654a8eae237e8594322e92
Eduard Toma <eduard.daniel.toma@gmail.com>
2019-04-04 01:45:01 +0300

    domain.persistence.service.transfer.test

---
+++ src/main/java/org/fasttrackit/onlineshopapi/persistence/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.fasttrackit.onlineshopapi.persistence;
+
+import org.fasttrackit.onlineshopapi.domain.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+// Long is wrapper class for primitive long
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+
+}
